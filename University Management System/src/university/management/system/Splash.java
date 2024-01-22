6d9f2d9038afd420bd1396ab1cd2ba3faac66a18
Cₕₐᵣᵤₙᵢ ₐbₑyₛᵢₙgₕₑ <126559529+CharuniAbeysinghe@users.noreply.github.com>
2024-01-22 15:09:24 +0530

    Add files via upload

---
+++ University Management System/src/university/management/system/Splash.java
@@ -0,0 +1,14 @@
+/*package university.management.system;
+
+public class Splash {
+    
+    Splash () {
+    
+    
+    
+        
+    public static void main(String args[]){
+            new Splash();
+    }    
+}
+*/
